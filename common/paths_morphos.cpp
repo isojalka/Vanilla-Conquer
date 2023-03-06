3d667208cd6dc68dbe1450efea7a72b75e2976d3
Mark Olsen <git@molsen.co.za>
2023-03-06 15:27:38 +0000

    MorphOS port.

--- common/paths_morphos.cpp
+++ common/paths_morphos.cpp
@@ -1,77 +1,13 @@
-// TiberianDawn.DLL and RedAlert.dll and corresponding source code is free
-// software: you can redistribute it and/or modify it under the terms of
-// the GNU General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
-
-// TiberianDawn.DLL and RedAlert.dll and corresponding source code is distributed
-// in the hope that it will be useful, but with permitted additional restrictions
-// under Section 7 of the GPL. See the GNU General Public License in LICENSE.TXT
-// distributed with this program. You should have received a copy of the
-// GNU General Public License along with permitted additional restrictions
-// with this program. If not, see https://github.com/electronicarts/CnC_Remastered_Collection
 #include <proto/dos.h>
 
+#include <string.h>
+#include <stdlib.h>
+
 #include "paths.h"
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-const char* PathsClass::Program_Path()
+std::string PathsClass::Argv_Path(const char* cmd_arg)
 {
-    if (ProgramPath.empty()) {
-        ProgramPath = "PROGDIR:";
-    }
-
-    return ProgramPath.c_str();
-}
-
-const char* PathsClass::Data_Path()
-{
-    if (DataPath.empty()) {
-        DataPath = "PROGDIR:data/";
-    }
-
-    return DataPath.c_str();
-}
-
-const char* PathsClass::User_Path()
-{
-    if (UserPath.empty()) {
-        UserPath = "PROGDIR:user/";
-    }
-
-    return UserPath.c_str();
-}
-
-bool PathsClass::Create_Directory(const char* dirname)
-{
-    bool ret = true;
-
-    if (dirname == nullptr) {
-        return ret;
-    }
-
-    std::string temp = dirname;
-    size_t pos = 0;
-    do {
-        pos = temp.find_first_of("/", pos + 1);
-        if (mkdir(temp.substr(0, pos).c_str(), 0700) != 0) {
-            if (errno != EEXIST) {
-                ret = false;
-                break;
-            }
-        }
-    } while (pos != std::string::npos);
-
-    return ret;
-}
-
-bool PathsClass::Is_Absolute(const char* path)
-{
-    return path != nullptr && strchr(path, ':') != 0;
+    return std::string("PROGDIR:");
 }
 
 std::string PathsClass::Concatenate_Paths(const char* path1, const char* path2)
@@ -92,12 +28,75 @@ std::string PathsClass::Concatenate_Paths(const char* path1, const char* path2)
     }
 }
 
-std::string PathsClass::Get_Filename(const char *path)
+bool PathsClass::Create_Directory(const char* dirname)
+{
+    bool ret = true;
+    char* tempname;
+    size_t namelen;
+    BPTR lock;
+    size_t i;
+
+    if (dirname == nullptr) {
+        return ret;
+    }
+
+    tempname = strdup(dirname);
+    if (tempname) {
+        namelen = strlen(tempname);
+        for (i = 0; i <= namelen; i++) {
+            if (tempname[i] == '/' || tempname[i] == 0) {
+                tempname[i] = 0;
+
+                if ((lock = CreateDir(tempname))) {
+                    UnLock(lock);
+                } else if (IoErr() != ERROR_OBJECT_EXISTS) {
+                    ret = false;
+                    break;
+                }
+
+                tempname[i] = '/';
+            }
+        }
+
+        free(tempname);
+    }
+
+    return ret;
+}
+
+const char* PathsClass::Data_Path()
+{
+    if (DataPath.empty()) {
+        DataPath = "PROGDIR:data/";
+    }
+
+    return DataPath.c_str();
+}
+
+std::string PathsClass::Get_Filename(const char* path)
 {
     return FilePart(path);
 }
 
-std::string PathsClass::Argv_Path(const char* cmd_arg)
+bool PathsClass::Is_Absolute(const char* path)
 {
-    return std::string("PROGDIR:");
+    return path != nullptr && strchr(path, ':') != 0;
+}
+
+const char* PathsClass::Program_Path()
+{
+    if (ProgramPath.empty()) {
+        ProgramPath = "PROGDIR:";
+    }
+
+    return ProgramPath.c_str();
+}
+
+const char* PathsClass::User_Path()
+{
+    if (UserPath.empty()) {
+        UserPath = "PROGDIR:user/";
+    }
+
+    return UserPath.c_str();
 }
